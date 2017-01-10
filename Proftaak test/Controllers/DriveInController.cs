ce59d45258d697fada7e4498bf6456c5b2076832
Jordy Verlaek <j.verlaek@student.fontys.nl>
2017-01-11 00:36:09 +0100

    In/uitrij section added and layout made, yet to be implemented

---
+++ Proftaak test/Controllers/DriveInController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proftaak_test.Controllers
+{
+    public class DriveInController : Controller
+    {
+        // GET: DriveIn
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult GetSectorForTram()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
