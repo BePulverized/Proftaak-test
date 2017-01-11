46a4015dd37af2ae1f55a5fee44d796573fd6f31
Putters,Job J <j.putters@student.fontys.nl>
2017-01-11 12:01:48 +0100

    Made a start with google maps

---
+++ Proftaak test/Controllers/MapsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proftaak_test.Controllers
+{
+    public class MapsController : Controller
+    {
+        // GET: Maps
+        public ActionResult Index()
+        {
+            if (Session["curEmployeeID"] == null)
+            {
+                return RedirectToAction("Login", "Employee");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
